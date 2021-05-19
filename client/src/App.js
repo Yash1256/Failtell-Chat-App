728dbb5e1c3260e5e0f91ee1f829f41190f5515f
priyanshu-678 <mpriyanshu263@gmail.com>
2021-05-19 10:19:17 +0530

    basic frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Component } from "react";
 import socketClient from "socket.io-client";
+import { Navbar } from "./components/navbar/navbar.component";
+import { FriendFilter } from "./components/searchBar/search.component";
+import { LeftBlockUserCard } from "./components/userBlock/userBlock.component";
+import { P2PChatBox } from "./components/chatBox/chatBox.component";
 
 const SERVER = "http://127.0.0.1:5000";
-function App() {
-  var socket = socketClient(SERVER);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var socket = socketClient(SERVER);
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="appBody">
+          <div className="leftBlock">
+            {/* <FriendFilter /> */}
+            <LeftBlockUserCard />
+          </div>
+          <div className="rightBlock">
+            <P2PChatBox />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/components/navbar/navbar.component.jsx
@@ -0,0 +1,10 @@
+import './navbar.style.css'
+import { NavbarSearchBar } from './../searchBar/search.component'
+
+export const Navbar = ()=>{
+        return (<div className="navbar"> 
+        <NavbarSearchBar />
+        </div>)
+}
+
+
