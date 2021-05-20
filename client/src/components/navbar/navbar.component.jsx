0fcf73ec8e5454a1ba0e1bdf31920cea7eaf49fc
priyanshu-678 <mpriyanshu263@gmail.com>
2021-05-20 14:55:05 +0530

    authenticated part

--- client/src/components/navbar/navbar.component.jsx
+++ client/src/components/navbar/navbar.component.jsx
@@ -1,10 +1,1 @@
-import './navbar.style.css'
-import { NavbarSearchBar } from './../searchBar/search.component'
-
-export const Navbar = ()=>{
-        return (<div className="navbar"> 
-        <NavbarSearchBar />
-        </div>)
-}
-
-
+import axios from 'axios'
